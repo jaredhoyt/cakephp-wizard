6915c99d6294a76a62f1520117be9305db8e8278
Dean Sofer <proloser@hotmail.com>
2012-02-21 01:03:21 -0800

    Merging master branch from @kindred

--- controllers/components/wizard.php
+++ controllers/components/wizard.php
@@ -404,6 +404,21 @@ class WizardComponent extends Object {
 		}		
 		$this->Session->write("$this->_sessionKey.$step", $data);
 	}
+	
+/**
+ * Resets the data from the Session that has been stored by the WizardComponent.
+ *
+ * @param mixed $name The name of the session variable (or a path as sent to Set.extract)
+ * @access public
+ */
+	function delete($key = null) {
+		if ($key == null) {
+			return;
+		} else {
+			$this->Session->delete("$this->_sessionKey.$key");
+			return;
+		}
+	}
 /**
  * Removes a branch from the steps array.
  *
