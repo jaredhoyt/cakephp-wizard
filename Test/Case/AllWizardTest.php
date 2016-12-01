c66585467b156b6a3972aac67f3064576694de25
bancer <val@dinnerbooking.com>
2016-12-01 13:49:19 +0100

    unit tests

--- Test/Case/AllWizardTest.php
+++ Test/Case/AllWizardTest.php
@@ -4,6 +4,7 @@ class AllWizardTest extends CakeTestSuite {
 	public static function suite() {
 		$suite = new CakeTestSuite('All Wizard tests');
 		$suite->addTestDirectoryRecursive(dirname(__FILE__) . DS . 'Controller');
+		$suite->addTestDirectoryRecursive(dirname(__FILE__) . DS . 'View');
 		return $suite;
 	}
 }
