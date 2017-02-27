a8981164fdd8e7e800041d0abb7879700572f5d5
Val Bancer <valerij.bancer@gmail.com>
2017-02-27 20:32:40 +0100

    Wizard root session key is refactored to a configurable component variable (#22)
    * Wizard root session key is refactored to a configurable component
    variable.
    
    * fixed unit test
    
    * Moved sessionRootKey related code from initialize to startup to ease the
    configuration in a controller.
    
    * Setting session keys refactored to a private method.
    
    * Fixed variable name.

--- Test/Case/Controller/Component/WizardComponentTest.php
+++ Test/Case/Controller/Component/WizardComponentTest.php
@@ -230,7 +230,7 @@ class WizardComponentTest extends CakeTestCase {
 		$this->assertEquals($expectedSteps, $resultSteps);
 		$this->assertEquals($expectedSteps, $this->Wizard->steps);
 		$expectedHelpers = array(
-			'Wizard.Wizard',
+			'Wizard.Wizard' => array('sessionRootKey' => 'Wizard'),
 		);
 		$this->assertEquals($expectedHelpers, $this->Wizard->controller->helpers);
 	}
@@ -273,6 +273,36 @@ class WizardComponentTest extends CakeTestCase {
 		$resultSteps = $this->Wizard->Session->read('Wizard.config.steps');
 		$this->assertEquals($expectedSteps, $resultSteps);
 		$this->assertEquals($expectedSteps, $this->Wizard->steps);
+	}
+
+	public function testStartupCustomRootSessionKey() {
+		$configAction = $this->Wizard->Session->read('WizardInstance001.config.action');
+		$this->assertEmpty($configAction);
+		$configSteps = $this->Wizard->Session->read('WizardInstance001.config.steps');
+		$this->assertEmpty($configSteps);
+		$this->assertEmpty($this->Wizard->controller->helpers);
+
+		$this->Wizard->sessionRootKey = 'WizardInstance001';
+		$this->Wizard->startup($this->Controller);
+
+		$expectedAction = 'wizard';
+		$resultAction = $this->Wizard->Session->read('WizardInstance001.config.action');
+		$this->assertEquals($expectedAction, $resultAction);
+		$expectedSteps = array(
+			'step1',
+			'step2',
+			'gender',
+			'step3',
+			'step4',
+			'confirmation',
+		);
+		$resultSteps = $this->Wizard->Session->read('WizardInstance001.config.steps');
+		$this->assertEquals($expectedSteps, $resultSteps);
+		$this->assertEquals($expectedSteps, $this->Wizard->steps);
+		$expectedHelpers = array(
+			'Wizard.Wizard' => array('sessionRootKey' => 'WizardInstance001'),
+		);
+		$this->assertEquals($expectedHelpers, $this->Wizard->controller->helpers);
 	}
 
 	public function testProcessStepOneGet() {
